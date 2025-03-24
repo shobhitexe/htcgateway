744db74b5a32709da354ff26c696657b553c7ecf
shobhitexe <sguptamds94@gmail.com>
2025-03-24 14:44:20 +0530

    Test

--- src/components/Checkout/CheckoutForm.tsx
+++ src/components/Checkout/CheckoutForm.tsx
@@ -267,8 +267,7 @@ export default function CheckoutForm({
                 id="checkout-total"
                 data-value={(Number(price) * (1 - discount / 100)).toFixed(2)}
               >
-                {/* ${(Number(price) * (1 - discount / 100)).toFixed(2)} */}
-                $1000
+                ${(Number(price) * (1 - discount / 100)).toFixed(2)}
               </div>
             </div>
 
