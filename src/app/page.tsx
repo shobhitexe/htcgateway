36c99668b268d622570bfd922e6089eaf4d70e1f
shobhitexe <sguptamds94@gmail.com>
2025-03-26 00:06:31 +0530

    Redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,47 @@
-import CheckoutForm from "@/components/Checkout/CheckoutForm";
-import React from "react";
+// import CheckoutForm from "@/components/Checkout/CheckoutForm";
+import { redirect } from "next/navigation";
 
-const config: Record<string, number>[] = [
-  {
-    "5000": 25,
-    "10000": 39,
-    "25000": 129,
-    "50000": 239,
-    "100000": 479,
-    "200000": 879,
-    "300000": 1199,
-  },
-  {
-    "5000": 25,
-    "10000": 39,
-    "25000": 129,
-    "50000": 239,
-    "100000": 479,
-    "200000": 879,
-    "300000": 1199,
-  },
-];
+// const config: Record<string, number>[] = [
+//   {
+//     "5000": 25,
+//     "10000": 39,
+//     "25000": 129,
+//     "50000": 239,
+//     "100000": 479,
+//     "200000": 879,
+//     "300000": 1199,
+//   },
+//   {
+//     "5000": 25,
+//     "10000": 39,
+//     "25000": 129,
+//     "50000": 239,
+//     "100000": 479,
+//     "200000": 879,
+//     "300000": 1199,
+//   },
+// ];
 
 interface PageProps {
   searchParams: Promise<Record<string, string | undefined>>;
 }
 
 export default async function page({ searchParams }: PageProps) {
-  const { step, account } = await searchParams;
+  console.log(searchParams);
 
-  if (!step || !account) {
-    return (
-      <div className="flex justify-center py-20 text-white">Invalid Config</div>
-    );
-  }
-
-  return (
-    <CheckoutForm step={step} account={account} price={config[0][account]} />
+  return redirect(
+    `https://platform.heratradingcapital.com/register?demo=false&competitions=false`
   );
+
+  // const { step, account } = await searchParams;
+
+  // if (!step || !account) {
+  //   return (
+  //     <div className="flex justify-center py-20 text-white">Invalid Config</div>
+  //   );
+  // }
+
+  // return (
+  //   <CheckoutForm step={step} account={account} price={config[0][account]} />
+  // );
 }
